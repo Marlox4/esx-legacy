3003107ee04f124160b238dcb3bc63750ce0319e
Marcel <70572904+Marlox4@users.noreply.github.com>
2022-05-11 15:52:33 +0200

    Update fxmanifest.lua

--- [esx]/es_extended/fxmanifest.lua
+++ [esx]/es_extended/fxmanifest.lua
@@ -18,31 +18,16 @@ shared_scripts {
 server_scripts {
 	'@oxmysql/lib/MySQL.lua',
 
-	'server/common.lua',
 	'server/classes/player.lua',
-	'server/functions.lua',
-	'server/onesync.lua',
-	'server/paycheck.lua',
-	'server/main.lua',
-	'server/commands.lua',
-
+	'server/*.lua',
 	'common/modules/math.lua',
 	'common/modules/table.lua',
 	'common/functions.lua'
 }
 
 client_scripts {
-	'client/common.lua',
-	'client/functions.lua',
-	'client/wrapper.lua',
-	'client/main.lua',
-
-	'client/modules/death.lua',
-	'client/modules/scaleform.lua',
-	'client/modules/streaming.lua',
-
-	'common/modules/math.lua',
-	'common/modules/table.lua',
+	'client/*.lua',
+	'client/modules/*.lua',
 	'common/functions.lua'
 }
 
@@ -56,17 +41,9 @@ files {
 	'html/ui.html',
 
 	'html/css/app.css',
-
-	'html/js/mustache.min.js',
-	'html/js/wrapper.js',
-	'html/js/app.js',
-
-	'html/fonts/pdown.ttf',
-	'html/fonts/bankgothic.ttf',
-
-	'html/img/accounts/bank.png',
-	'html/img/accounts/black_money.png',
-	'html/img/accounts/money.png'
+	'html/js/*.js',
+	'html/fonts/*.ttf',
+	'html/img/accounts/*.png',
 }
 
 dependencies {
